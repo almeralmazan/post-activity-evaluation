841c119f5c890fe845f970d206001bf093dc9a42
Almer Almazan <almeralmazan@gmail.com>
2012-05-28 13:20:09 +0800

    added success and failed pages with sample records

--- app.rb
+++ app.rb
@@ -44,11 +44,48 @@ end
 
 # create record
 post '/create' do
+  p = Person.new
+  p.name = params[:full_name]
+  p.age = params[:age]
+  p.sex = params[:sex]
+  p.work = params[:work]
+  p.address = params[:address]
+  p.benefit = params[:benefits]
+  p.first_answer = params[:first_answer]
+  p.second_answer = params[:second_answer]
+  p.third_answer = params[:third_answer]
+  p.fourth_answer = params[:fourth_answer]
+  p.created_at = Time.now
 
+  if p.save
+    status 201
+    redirect '/success'
+  else
+    status 412
+    redirect '/failed'
+  end
 end
 
-post '/register' do
-  @title = 'Register Page'
-  @name = params[:full_name]      # this is only for test
-  haml :register
+get '/success' do
+  erb :success
+end
+
+get '/failed' do
+  erb :failed
+end
+
+# list
+post '/test' do
+  @name = params[:full_name]
+  @age = params[:age]
+  @sex = params[:sex] 
+  @work = params[:work] 
+  @address = params[:address] 
+  @benefit = params[:benefits] 
+  @first_answer = params[:first_answer] 
+  @second_answer = params[:second_answer] 
+  @third_answer = params[:third_answer] 
+  @fourth_answer = params[:fourth_answer] 
+  @created_at = Time.now
+  erb :test
 end
