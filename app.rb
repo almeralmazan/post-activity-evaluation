3821a29cde1ea0b94d6f40df27d3ed3cbe394693
Almer Almazan <almeralmazan@gmail.com>
2012-06-06 10:04:16 +0800

    remove people.xls and pae.db file

--- app.rb
+++ app.rb
@@ -18,18 +18,18 @@ end
 
 # create record
 post '/create' do
-  p = Person.new
-  p.name = params[:full_name]
-  p.age = params[:age]
-  p.sex = params[:sex]
-  p.work = params[:work]
-  p.address = params[:address]
-  p.benefit = params[:benefits]
-  p.first_answer = params[:first_answer]
+  p               = Person.new
+  p.name          = params[:full_name]
+  p.age           = params[:age]
+  p.sex           = params[:sex]
+  p.work          = params[:work]
+  p.address       = params[:address]
+  p.benefit       = params[:benefits]
+  p.first_answer  = params[:first_answer]
   p.second_answer = params[:second_answer]
-  p.third_answer = params[:third_answer]
+  p.third_answer  = params[:third_answer]
   p.fourth_answer = params[:fourth_answer]
-  p.created_at = Time.now
+  p.created_at    = Time.now
 
   if p.save
     status 201
@@ -40,12 +40,18 @@ post '/create' do
   end
 end
 
-get '/success' do
-  @title = "Success Page"
-  erb :success
+# list all person
+get '/list' do
+  @persons = Person.all
+  haml :list
 end
 
-get '/failed' do
-  @title = "Failed Page"
-  erb :failed
-end
+# get '/success' do
+#   @title = "Success Page"
+#   erb :success
+# end
+# 
+# get '/failed' do
+#   @title = "Failed Page"
+#   erb :failed
+# end
